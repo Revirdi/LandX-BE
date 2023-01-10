fe1e6b62b516702f53587d548fb1e6db04734be5
Revirdi <ardwiyanrevirdi99@gmail.com>
2023-01-10 13:37:57 +0700

    first commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('api/user')
+export class UserController {
+  constructor(private userService: UserService) {}
+  @Post()
+  addUser() {
+    return 'lah';
+  }
+}
